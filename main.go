efbd7252f5060895605f3ff0983dff17d3bc6a83
icemining <mail@icemining.io>
2017-01-09 21:13:35 +0100

    Rewrite sammy007 to icemining & open-ethereum-pool to ethereum-pool

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 
 	"github.com/yvasiyarov/gorelic"
 
-	"github.com/sammy007/open-ethereum-pool/api"
-	"github.com/sammy007/open-ethereum-pool/payouts"
-	"github.com/sammy007/open-ethereum-pool/proxy"
-	"github.com/sammy007/open-ethereum-pool/storage"
+	"github.com/icemining/ethereum-pool/api"
+	"github.com/icemining/ethereum-pool/payouts"
+	"github.com/icemining/ethereum-pool/proxy"
+	"github.com/icemining/ethereum-pool/storage"
 )
 
 var cfg proxy.Config
