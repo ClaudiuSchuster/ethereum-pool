24013df24fabe42a10a9fc5fa3663ee69c505a3c
icemining-eu2 <mail@icemining.io>
2017-01-19 20:43:09 +0100

    format adresses

---
+++ www/tests/unit/helpers/format-addr-test.js
@@ -0,0 +1,12 @@
+
+import { formatAddr } from 'ethereum-pool/helpers/format-addr';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format addr');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatAddr([42]);
+  assert.ok(result);
+});
+
