6b21aa90c8353a8b17386adca768d0055d94981f
icemining-eu2 <mail@icemining.io>
2017-01-20 21:42:32 +0100

    changed api-server: added payment to StatsIndex

--- api/server.go
+++ api/server.go
@@ -172,6 +172,7 @@ func (s *ApiServer) StatsIndex(w http.ResponseWriter, r *http.Request) {
 		reply["maturedTotal"] = stats["maturedTotal"]
 		reply["immatureTotal"] = stats["immatureTotal"]
 		reply["candidatesTotal"] = stats["candidatesTotal"]
+		reply["payments"] = stats["payments"]
 		reply["paymentsTotal"] = stats["paymentsTotal"]
 	}
 
