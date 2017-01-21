bfa8c42d972c2457ea62f465c11e21bff6bba240
icemining-eu2 <mail@icemining.io>
2017-01-21 17:12:47 +0100

    introduced "format-dyn for blocks, maybe temporarly...

---
+++ www/app/helpers/format-dyn.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function formatDyn(value) {
+  return value[0].substring(value[1], value[2]) + "..." + value[0].substring(value[3]);
+}
+
+export default Ember.Helper.helper(formatDyn);
